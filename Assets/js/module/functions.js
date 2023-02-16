9ad44803b53975567cb18a55a07df85c20149497
Johan Zuluaga <johan16zulu@gmail.com>
2023-02-15 22:35:00 -0500

    implementacion carrusel

--- Assets/js/module/functions.js
+++ Assets/js/module/functions.js
@@ -9,13 +9,18 @@ export async function getData() {
   }
 }
 
-export function createCards(list, container) {
+export function createCards(list, container,formulario) {
   container.innerHTML = ""
   let aux = ""
-  for (let element of list) {
-      aux += writeCard(element)
+  if(list.length === 0){
+    noEncontrado(container,formulario)
+  }else{
+
+    for (let element of list) {
+        aux += writeCard(element)
+    }
+    container.innerHTML += aux
   }
-  container.innerHTML += aux
 }
 
 export function writeCard(element) {
@@ -24,7 +29,7 @@ export function writeCard(element) {
     <div class="card" style="width: 18rem;">
         <div>
             <img src="${element.imagen}"class="card-img-top tam_img_card" alt="${element.producto}">
-            <button class="favorite-btn"><img src="../img/heart-svgrepo-com.png" class="img-heart" alt="heart"></button>
+            <button class="favorite-btn"><img src="../img/heart.png" class="img-heart" alt="heart"></button>
         </div>
         <div class="card-body ">
             <div class="cont-stock d-flex flex-column">
@@ -44,3 +49,47 @@ export function writeCard(element) {
 export function filterProducts(products, value){
   return products.filter( (product) => product.producto.toLowerCase().includes(value))
 }
+
+export function writeSponsors(list, container){
+    for(let i=0; i<18; i++){
+      container.innerHTML+=`
+        <div class="slide">
+          <img src="../images/${list[i]}" alt="">
+        </div>
+      `
+    }
+}
+
+export function createCarru(list, container){
+  
+    container.innerHTML+=`
+      <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img src="../img/${list[0]}" class="d-block w-100 border1" alt="${list[0]}">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="../img/${list[1]}" class="d-block w-100 border1" alt="${list[1]}">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="../img/${list[2]}" class="d-block w-100 border1" alt="${list[2]}">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="../img/${list[3]}" class="d-block w-100 border1" alt="${list[3]}">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="../img/${list[4]}" class="d-block w-100 border1" alt="${list[4]}">
+                      </div>
+                    </div>
+    `
+  
+}
+
+export function noEncontrado(container,formulario){
+  container.innerHTML = `<div class="style-mens">
+  <h5>No ha sido posible encontrar nada para "${formulario}".</h5>
+  <h5>Prueba a detallar tu búsqueda.</h5>
+  <img src="../img/cinnamon.png" alt="perro">
+  </div>`
+  
+}
+
